0afdeff3db9d7f33bcc60fe9943c03708ac5a903
makenziemgray <makenziemgray0102@gmail.com>
2025-07-05 15:39:59 -0400

    using OOP and logic into an object that can be represented as a single variable.

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
+const taskDescription = [];
+const tasks = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
-}
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+  };
+  tasks.push(task);
 
+}
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
 function completeTask(taskIndex) {
   taskComplete[taskIndex] = true;
@@ -19,6 +25,10 @@ function logTaskState(taskIndex) {
   const title = taskTitles[taskIndex];
   const complete = taskComplete[taskIndex];
   console.log(`${title} has${complete ? " " : " not "}been completed`);
+}
+
+function logAllTasks() {
+  tasks.forEach((task, index) => logTaskState(index));
 }
 
 // DRIVER CODE BELOW
